8b6d0cd09ae63e3d5fd791dc06532410634dee97
jihazard <yoonjh238@gmail.com>
2019-02-22 14:17:17 +0900

    선택정렬과 버블정렬 알고리즘 연습

---
+++ java_Study/src/algo/BubbleSort.java
@@ -0,0 +1,29 @@
+package algo;
+/*버블 정렬
+* 옆에 있는 값과 비교해서 더 작은 값을 아으로 보내면 어떨까?
+* 구현은 쉬우나 효율은 가장 비효율적
+* */
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] value = {10,9,8,7,6,5,4,3,2,1} ;
+        System.out.println(arr(value));
+    }
+
+    public static int[] arr(int[] demoArray){
+        int temp;
+
+        for (int i = 0; i < demoArray.length; i++) {
+            for (int j = 0; j < 9-i; j++) {
+                if(demoArray[j] > demoArray[j+1]){
+                    temp = demoArray[j];
+                    demoArray[j] = demoArray[j+1];
+                    demoArray[j+1] = temp;
+                }
+            }
+        }
+
+
+        for (int x : demoArray) System.out.print(x +" , ");
+        return demoArray;
+    }
+}
---
+++ java_Study/src/algo/SelectionSort.java
@@ -0,0 +1,30 @@
+package algo;
+/*선택정렬*/
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] value = {10,9,8,7,6,5,4,3,2,1} ;
+        System.out.println(arr(value));
+    }
+
+    public static int[] arr(int[] demoArray){
+        int min , temp ,  index = 0;
+        for (int i = 0; i < demoArray.length ; i++) {
+            min = 9999;
+
+            for (int j = i; j < demoArray.length; j++) {
+                if(min > demoArray[j]) {
+                    min = demoArray[j];
+                    index = j;
+                }
+            }
+            temp = demoArray[i];
+            demoArray[i] = demoArray[index];
+            demoArray[index] = temp;
+        }
+
+
+
+        for (int x : demoArray) System.out.print(x +" , ");
+        return demoArray;
+    }
+}

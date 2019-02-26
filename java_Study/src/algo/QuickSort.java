455be216868b2d43643b9b6b96517740dcd069c3
jihazard <yoonjh238@gmail.com>
2019-02-26 20:21:15 +0900

    퀵정렬

---
+++ java_Study/src/algo/QuickSort.java
@@ -0,0 +1,44 @@
+package algo;
+
+public class QuickSort {
+    public static void main(String[] args) {
+        int number = 10;
+        int[] value = {10,9,3,7,6,5,4,8,2,1} ;
+        for (int x : value
+        ) {
+            System.out.print(x + ",");
+        }
+
+        quickSort(value,0, number-1);
+        for (int x : value
+             ) {
+            System.out.print(x + ",");
+        }
+    }
+
+    public static void quickSort(int[] data , int left , int right){
+
+        int pivot = data[(left+ right)/2];
+        System.out.println("----피벗값 : " + pivot);
+        do {
+            while (data[left] > pivot) left++;
+            while (data[right] < pivot) right--;
+            System.out.println("레프트 : " + left +"///// 라이트 : " + right);
+
+            if (left <= right) {
+                int temp = data[left];
+                data[left] = data[right];
+                data[right] = temp;
+
+                left++;
+                right--;
+
+            }
+        }while (left <= right) ;
+
+            if (left < right) quickSort(data, left, right);
+            if (right > left) quickSort(data, left, right);
+        }
+
+
+}

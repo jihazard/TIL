ef2e6f755edde083a4834db7a7b72a6cd91bcef3
jihazard <yoonjh238@gmail.com>
2019-03-06 11:53:15 +0900

    별만들기1

---
+++ java_Study/src/algo/_16_makeStar/Main.java
@@ -0,0 +1,20 @@
+package algo._16_makeStar;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("-----------------별 숫자를 입력하세요");
+        int n = sc.nextInt();
+
+           for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i+1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+    }
+}

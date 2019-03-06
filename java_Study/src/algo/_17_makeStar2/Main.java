990e71bb7613d6509f38276ae234e91daf48180e
jihazard <yoonjh238@gmail.com>
2019-03-06 15:57:56 +0900

    별 만들기 2

---
+++ java_Study/src/algo/_17_makeStar2/Main.java
@@ -0,0 +1,16 @@
+package algo._17_makeStar2;
+
+public class Main {
+    public static void main(String[] args) {
+        int n=5;
+
+        for (int i = 0; i < n ; i++) {
+            for (int j = 0; j < n ; j++) {
+
+            if(i>j) System.out.print(" ");
+            else System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}

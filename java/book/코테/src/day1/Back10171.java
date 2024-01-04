a5db0fe8adb714b7a807f3e5373d3c65fed9d6dd
yoonjh238@gmail.com <github@incorelab.co.kr>
2024-01-04 10:18:39 +0900

    코테 연습문제 10171 고양이

---
+++ java/book/코테/src/day1/Back10171.java
@@ -0,0 +1,37 @@
+package day1;
+
+
+
+/*
+문제
+아래 예제와 같이 고양이를 출력하시오.
+
+입력
+없음.
+
+출력
+고양이를 출력한다.
+
+예제
+\    /\
+ )  ( ')
+(  /  )
+ \(__)|
+
+//  \, ' 등의 문자에 주의하며 고양이를 출력하는 문제
+
+* */
+
+import java.util.Scanner;
+
+public class Back10171 {
+	public static void main(String args[]){
+		String cat = "\\    /\\\n"
+			+ " )  ( ')\n"
+			+ "(  /  )\n"
+			+ " \\(__)|";
+
+		System.out.println(cat);
+
+	}
+}

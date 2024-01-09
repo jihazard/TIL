021c3c9767899dc56a00249dcba7f088222831a7
yoonjh238@gmail.com <github@incorelab.co.kr>
2024-01-09 10:42:59 +0900

    코테 연습문제

---
+++ java/book/코테/src/day2/Back5597.java
@@ -0,0 +1,60 @@
+package day2;
+
+/*
+
+문제
+X대학 M교수님은 프로그래밍 수업을 맡고 있다. 교실엔 학생이 30명이 있는데, 학생 명부엔 각 학생별로 1번부터 30번까지 출석번호가 붙어 있다.
+
+교수님이 내준 특별과제를 28명이 제출했는데, 그 중에서 제출 안 한 학생 2명의 출석번호를 구하는 프로그램을 작성하시오.
+
+입력
+입력은 총 28줄로 각 제출자(학생)의 출석번호 n(1 ≤ n ≤ 30)가 한 줄에 하나씩 주어진다. 출석번호에 중복은 없다.
+
+출력
+출력은 2줄이다. 1번째 줄엔 제출하지 않은 학생의 출석번호 중 가장 작은 것을 출력하고, 2번째 줄에선 그 다음 출석번호를 출력한다.
+
+1.set을 이용하여 간단하게 처리
+2.배열을 이요하여 처리
+
+* */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Back5597 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int DEFAULT_COUNT = 30;
+        //Set<Integer> set = new HashSet<>();
+        int[] arr = new int[DEFAULT_COUNT];
+        StringBuilder builder = new StringBuilder();
+        for(int i=0; i< DEFAULT_COUNT; i++){
+            int temp = i + 1;
+            //set.add(temp);
+            arr[i] = temp;
+        }
+
+        for(int i=0; i< DEFAULT_COUNT - 2; i++){
+            int temp = Integer.parseInt(br.readLine());
+            for(int j=0; j<arr.length; j++){
+                if(arr[j] == temp){
+                   arr[j] = 0;
+                }
+            }
+        }
+
+        List<Integer> list = new ArrayList<>();
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] != 0){
+                list.add(arr[i]);
+            }
+        }
+        Collections.sort(list);
+        builder.append(list.get(0)).append("\n").append(list.get(1));
+        System.out.println(builder.toString());
+    }
+}
+

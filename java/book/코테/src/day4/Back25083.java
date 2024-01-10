0c4f14c23337791337b28447d0ec60f26eda1a46
yoonjh238@gmail.com <github@incorelab.co.kr>
2024-01-10 15:44:52 +0900

    심화 체스

---
+++ java/book/코테/src/day4/Back25083.java
@@ -0,0 +1,31 @@
+package day4;
+
+/*
+
+
+문제
+아래 예제와 같이 새싹을 출력하시오.
+
+입력
+입력은 없다.
+
+출력
+새싹을 출력한다.
+* */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Back25083 {
+	public static void main(String args[]) {
+		String leaf = "         ,r'\"7\n"
+			+ "r`-_   ,'  ,/\n"
+			+ " \\. \". L_r'\n"
+			+ "   `~\\/\n"
+			+ "      |\n"
+			+ "      |";
+		System.out.println(leaf);
+	}
+}
+

64914d73fa332581c2316a312e3cc24e4fa3527b
yoonjh238@gmail.com <github@incorelab.co.kr>
2024-01-23 17:57:57 +0900

    vue

--- java/강의/vue/source/demo1/vue/app.js
+++ java/강의/vue/source/demo1/vue/app.js
@@ -2,13 +2,15 @@ Vue.createApp({
     data() {
         return{
             goals :[],
-            enteredValue: ''
+            enteredValue: '',
+            count:0
         };
     },
     methods: {
         addGoal() {
             this.goals.push(this.enteredValue)
             this.enteredValue =''
+            this.count++;    
         }
     }
 }).mount('#app');
